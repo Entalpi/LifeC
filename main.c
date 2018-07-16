0dba2e72df02016ca8d8f5b477eb8cc8710cdde8
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2018-07-16 15:05:00 +0200

    Generation counter.

--- main.c
+++ main.c
@@ -10,24 +10,32 @@
 /* Number of microseconds per tick */
 #define TICK_DELAY 1600000
 
+/* Contains the application state */
+typedef struct {
+	uint64_t generation;
+	bool quit;
+	bool paused;
+	bool extinct;
+} GameState;
+
 WINDOW* init_ncurses(void);
-void handle_input(bool* quit);
-void update_board(bool* curr_iter, bool* next_iter, uint32_t w, uint32_t h);
-void display_board(bool* board, uint32_t w, uint32_t h);
+void handle_input(GameState* state);
+bool update_board(bool* curr_iter, bool* next_iter, uint32_t w, uint32_t h);
+void display_board(const bool* board, uint32_t w, uint32_t h);
+void display_ui(const GameState* game_state, uint32_t w, uint32_t h);
 void init_board(bool* board, uint32_t w, uint32_t h);
 int idx_at(uint32_t x, uint32_t y, uint32_t w, uint32_t h);
 
-void print_usage(void);
 void print_command_line_arguments(void);
 bool parse_arguments(int argc, char const* argv[]);
 
 int main(int argc, char const* argv[]) {
+	GameState game_state = {.generation = 1, .quit = false, .paused = false };
 	WINDOW* root = init_ncurses();
 	if (!parse_arguments(argc, argv)) {
 		print_command_line_arguments();
 		return 0;
 	}
-	print_usage();
 	const uint32_t w = 10;
 	const uint32_t h = 10;
 	bool* board0 = calloc(sizeof(bool), w * h);
@@ -36,15 +44,21 @@ int main(int argc, char const* argv[]) {
 	uint8_t current_board_idx = 0;
 	init_board(boards[current_board_idx], w, h);
 	display_board(boards[current_board_idx], w, h);
-	bool quit = false;
-	while (!quit) {
-		uint8_t next_board_idx = (current_board_idx + 1) % 2;
+	while (!game_state.quit) {
+		handle_input(&game_state);
+		if (!game_state.paused) {
+			uint8_t next_board_idx = (current_board_idx + 1) % 2;
 
-		handle_input(&quit);
-		update_board(boards[current_board_idx], boards[next_board_idx], w, h);
-		display_board(boards[current_board_idx], w, h);
-		
-		current_board_idx = next_board_idx;
+			if (update_board(boards[current_board_idx], boards[next_board_idx], w, h)) {
+				game_state.generation++;
+			} else {
+				game_state.extinct = true;
+			}
+			display_board(boards[current_board_idx], w, h);
+			
+			current_board_idx = next_board_idx;
+		}
+		display_ui(&game_state, w, h);
 		usleep(TICK_DELAY);
 	}
 	return 0;
@@ -59,7 +73,8 @@ WINDOW* init_ncurses(void) {
 	return root;
 }
 
-void update_board(bool* curr_iter, bool* next_iter, uint32_t w, uint32_t h) {
+bool update_board(bool* curr_iter, bool* next_iter, uint32_t w, uint32_t h) {
+	bool life_survived_to_next_gen = false;
 	for (uint32_t x = 0; x < w; x++) {
 		for (uint32_t y = 0; y < h; y++) {
 			uint8_t l = curr_iter[idx_at(x - 1, y, w, h)] ? 1 : 0;
@@ -86,19 +101,20 @@ void update_board(bool* curr_iter, bool* next_iter, uint32_t w, uint32_t h) {
 			if (neighbors < 2) {
 				next_iter[idx_at(x, y, w, h)] = false;
 			}
-			// Lives on
-			if (neighbors == 2 || neighbors == 3) {
-				continue;	
-			}
 			// Overpopulation
 			if (neighbors > 3) {
 				next_iter[idx_at(x, y, w, h)] = false;	
 			}
+			// Detect if the next generation exists at all
+			if (next_iter[idx_at(x, y, w, h)]) {
+				life_survived_to_next_gen = true;
+			}
 		}
 	}
+	return life_survived_to_next_gen;
 }
 
-void display_board(bool* board, uint32_t w, uint32_t h) {
+void display_board(const bool* board, uint32_t w, uint32_t h) {
 	for (uint32_t x = 0; x < w; x++) {
 		for (uint32_t y = 0; y < h; y++) {
 			if (board[idx_at(x, y, w, h)]) {
@@ -108,12 +124,17 @@ void display_board(bool* board, uint32_t w, uint32_t h) {
 			}
 		}
 	}
+}
+
+void display_ui(const GameState* game_state, uint32_t w, uint32_t h) {
+	mvprintw(h, 0, "Generation: %u", game_state->generation);
+	mvprintw(h + 1, 0, "Q: quit, P: pause/unpause");
 	refresh();
 }
 
 void init_board(bool* board, uint32_t w, uint32_t h) {
 	// Fill board randomly 
-	srand(time(NULL));
+	srand48(time(NULL));
 	for (uint32_t x = 0; x < w; x++) {
 		for (uint32_t y = 0; y < h; y++) {
 			board[idx_at(x, y, w, h)] = (drand48() < 0.2) ? true : false;
@@ -121,21 +142,20 @@ void init_board(bool* board, uint32_t w, uint32_t h) {
 	}
 }
 
-void handle_input(bool* quit) {
-	if (getch() == 'q') {
-		*quit = true;
+void handle_input(GameState* state) {
+	char ch = getch();
+	switch (ch) {
+		case 'q':
+			state->quit = true;
+			break;
+		case 'p':
+			state->paused = !state->paused;
+			break;
 	}
 }
 
 int idx_at(uint32_t x, uint32_t y, uint32_t w, uint32_t h) {
 	return (y % h) * w + (x % w);
-}
-
-void print_usage(void) {
-	int row = 0;
-	int col = 0;
-	getmaxyx(stdscr, row, col);
-	mvprintw(row, col, "Quit: q \n");
 }
 
 void print_command_line_arguments(void) {
